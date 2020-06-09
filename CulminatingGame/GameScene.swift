f812ea67f64a20aee32f97113e2d668bb6ce5f69
Keira Hartley <khartley@lcs.on.ca>
2020-06-09 09:41:42 -0400

    Got the orbs showing on the screen, with no working physics body

--- CulminatingGame/GameScene.swift
+++ CulminatingGame/GameScene.swift
@@ -9,81 +9,68 @@
 import SpriteKit
 import GameplayKit
 
-class GameScene: SKScene {
+class GameScene: SKScene, SKPhysicsContactDelegate {
     
-    private var label : SKLabelNode?
-    private var spinnyNode : SKShapeNode?
+    
+    
+    let scoreText = SKLabelNode(fontNamed: "AvenirNextCondensed-Bold")
+    var gameTimer: Timer?
+    var score = 0 {
+        didSet {
+            scoreText.text = "SCORE: \(score)"}
+    }
     
     override func didMove(to view: SKView) {
         
-        // Get label node from scene and store it for use later
-        self.label = self.childNode(withName: "//helloLabel") as? SKLabelNode
-        if let label = self.label {
-            label.alpha = 0.0
-            label.run(SKAction.fadeIn(withDuration: 2.0))
-        }
+        // set the background picture
+        let background = SKSpriteNode(imageNamed: "GameBackground")
+        background.zPosition = -10
+        addChild(background)
         
-        // Create shape node to use during mouse interaction
-        let w = (self.size.width + self.size.height) * 0.05
-        self.spinnyNode = SKShapeNode.init(rectOf: CGSize.init(width: w, height: w), cornerRadius: w * 0.3)
+        gameTimer = Timer.scheduledTimer(timeInterval: 0.35, target: self, selector: #selector(createOrb), userInfo: nil, repeats: true)
+      
+        // Makes the score label
+        scoreText.zPosition = 2
+        scoreText.position.y = 400
+        addChild(scoreText)
+        score = 0
         
-        if let spinnyNode = self.spinnyNode {
-            spinnyNode.lineWidth = 2.5
-            
-            spinnyNode.run(SKAction.repeatForever(SKAction.rotate(byAngle: CGFloat(Double.pi), duration: 1)))
-            spinnyNode.run(SKAction.sequence([SKAction.wait(forDuration: 0.5),
-                                              SKAction.fadeOut(withDuration: 0.5),
-                                              SKAction.removeFromParent()]))
-        }
     }
-    
     
     func touchDown(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.green
-            self.addChild(n)
-        }
-    }
-    
-    func touchMoved(toPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.blue
-            self.addChild(n)
-        }
+        
     }
     
     func touchUp(atPoint pos : CGPoint) {
-        if let n = self.spinnyNode?.copy() as! SKShapeNode? {
-            n.position = pos
-            n.strokeColor = SKColor.red
-            self.addChild(n)
-        }
-    }
-    
-    override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
-        if let label = self.label {
-            label.run(SKAction.init(named: "Pulse")!, withKey: "fadeInOut")
-        }
         
-        for t in touches { self.touchDown(atPoint: t.location(in: self)) }
     }
     
-    override func touchesMoved(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchMoved(toPoint: t.location(in: self)) }
-    }
-    
-    override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
-    override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
-        for t in touches { self.touchUp(atPoint: t.location(in: self)) }
-    }
-    
+    //      override func touchesBegan(_ touches: Set<UITouch>, with event: UIEvent?) {
+    //
+    //      }
+    //
+    //      override func touchesEnded(_ touches: Set<UITouch>, with event: UIEvent?) {
+    //
+    //      }
+    //
+    //      override func touchesCancelled(_ touches: Set<UITouch>, with event: UIEvent?) {
+    //      }
     
     override func update(_ currentTime: TimeInterval) {
         // Called before each frame is rendered
     }
+    
+    @objc func createOrb() {
+        // this method creates the orbs
+        let sprite = SKSpriteNode (imageNamed: "orb")
+        sprite.position = CGPoint(x: Int.random(in:-500...500), y: 300)
+        sprite.zPosition = 1
+        sprite.name = "orb"
+        addChild(sprite)
+        // add physics body
+        sprite.physicsBody = SKPhysicsBody(texture: sprite.texture!, size: sprite.size)
+        sprite.physicsBody?.velocity = CGVector(dx: 0, dy: -1)
+        sprite.physicsBody?.linearDamping = 0
+    }
+    
 }
